0e416c52de8b62f2e88db3d605a8df24b9f3c305
Pete Bosler <pabosle@sandia.gov>
2016-02-11 23:40:59 -0700

    started templated implementation.

---
+++ src/LpmXyzVector.hpp
@@ -0,0 +1,130 @@
+#ifndef __LPM_XyzVector__
+#define __LPM_XyzVector__
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include "GlobalConstants.h"
+
+template < typename scalarType > class XyzVector
+{
+	public :
+		scalarType x;
+		scalarType y;
+		scalarType z;
+		
+		XyzVector( const scalarType nx = 0.0, const scalarType ny = 0.0, const scalarType nz = 0.0 ) :
+			x(nx), y(ny), z(nz) {};
+		
+		~XyzVector() {};
+		
+		XyzVector operator += (const XyzVector& other) 
+			{ x += other.x;
+			  y += other.y;
+			  z += other.z;
+			  return *this;	}
+		XyzVector operator += (XyzVector& other)
+			{ x += other.x;
+			  y += other.y;
+			  z += other.z;
+			  return *this;	}
+		XyzVector operator -= (const XyzVector& other) 
+			{ x -= other.x;
+			  y -= other.y;
+			  z -= other.z;
+			  return *this;	}
+		XyzVector operator -= (XyzVector& other)
+			{ x -= other.x;
+			  y -= other.y;
+			  z -= other.z;
+			  return *this;	}
+		
+		scalarType magnitude() const { return std::sqrt( x*x + y*y + z*z); }
+		
+		scalarType dotProduct( const XyzVector& other) const { return x*other.x + y*other.y + z*other.z	; }
+		
+		XyzVector crossProduct( const XyzVector& other) const 
+			{ return XyzVector( y * other.z - z * other.y, z * other.x - x * other.z, x * other.y - y * other.x); }
+		
+		void scale( const scalarType multiplier ){ x *= multiplier; y *= multiplier; z *= multiplier; }
+		
+		void normalize() { const scalarType norm = magnitude(); x /= norm; y /= norm; z /= norm; }
+			
+};
+
+template <typename scalarType> XyzVector<scalarType> operator + ( const XyzVector<scalarType>& vecA, const XyzVector<scalarType>& vecB ) {
+	return XyzVector<scalarType>( vecA.x + vecB.x, vecA.y + vecB.y, vecA.z + vecB.z); }
+
+template <typename scalarType> XyzVector<scalarType> operator - ( const XyzVector<scalarType>& vecA, const XyzVector<scalarType>& vecB ) {
+	return XyzVector<scalarType>( vecA.x - vecB.x, vecA.y - vecB.y, vecA.z - vecB.z); }
+	
+template <typename scalarType> XyzVector<scalarType> operator * ( const XyzVector<scalarType>& vecA, const XyzVector<scalarType>& vecB ) {
+	return XyzVector<scalarType>( vecA.x * vecB.x, vecA.y * vecB.y, vecA.z * vecB.z); }
+
+template <typename scalarType> std::ostream& operator << ( std::ostream& os, const XyzVector<scalarType>& vec ) {
+	os << "(" << vec.x << ", " << vec.y << ", " << vec.z << ")\n" ; return os; }
+	
+template <typename scalarType> scalarType atan4( const scalarType y, const scalarType x ) {
+	static const scalarType PI = GlobalConstants::Instance()->Pi(); 
+	scalarType result = 0.0;
+	if ( x == 0.0 )
+	{
+		if ( y > 0.0 ) 
+			result = 0.5 * PI;
+		else if ( y < 0.0 )
+			result = 1.5 * PI;
+		else if ( y == 0.0 )
+			result = 0.0;
+	}
+	else if ( y == 0 )
+	{
+		if ( x > 0.0 )
+			result = 0.0;
+		else if ( x < 0.0 )
+			result = PI;
+	}
+	else
+	{
+		scalarType theta = std::atan2( std::abs(y), std::abs(x) );
+		if ( x > 0.0 && y > 0.0 )
+			result = theta;
+		else if ( x < 0.0 && y > 0.0 ) 
+			result = PI - theta;
+		else if ( x < 0.0 && y < 0.0 ) 
+			result = PI + theta;
+		else if ( x > 0.0 && y < 0.0 )
+			result = 2.0 * PI - theta;
+	}
+	return result;
+}
+
+template <typename scalarType> bool operator == ( const XyzVector<scalarType>& vecA, const XyzVector<scalarType>& vecB ){
+	static const scalarType	ZERO_TOL = GlobalConstants::Instance()->ZeroTol();
+	return ( ( std::abs( vecA.x - vecB.x ) < ZERO_TOL ) && ( std::abs(vecA.y - vecB.y) < ZERO_TOL)) && ( std::abs(vecA.z - vecB.z) < ZERO_TOL ) ;
+}
+
+template <typename scalarType> XyzVector<scalarType> midpoint( const XyzVector<scalarType>& vecA, const XyzVector<scalarType>& vecB ) {
+	return XyzVector<scalarType>( 0.5 * (vecA.x + vecB.x), 0.5 * (vecA.y + vecB.y), 0.5 * (vecA.z + vecB.z) );
+}
+
+template <typename scalarType> XyzVector<scalarType> centroid( const std::vector<XyzVector<scalarType> > vecs ) {
+	XyzVector<scalarType> cntd(0.0, 0.0, 0.0);
+	for ( int i = 0; i < vecs.size(); ++i)
+		cntd += vecs[i];
+	cntd.scale( 1.0 / vecs.size() );
+	return cntd;
+}
+
+template <typename scalarType> scalarType distance( const XyzVector<scalarType>& vecA, const XyzVector<scalarType>& vecB){
+	return std::sqrt( (vecB.x - vecA.x) * (vecB.x - vecA.x) + (vecB.y - vecA.y) * ( vecB.y - vecA.y) + (vecB.z - vecA.z) * (vecB.z - vecA.z));
+}
+
+//template <typename scalarType> scalarType triArea( const std::vector<XyzVector<scalarType> > vecs ) { }
+
+template <typename scalarType> scalarType triArea( const XyzVector<scalarType>& vecA, const XyzVector<scalarType>& vecB, const XyzVector<scalarType>& vecC){
+	XyzVector<scalarType> diff1 = vecB - vecA;
+	XyzVector<scalarType> diff2 = vecC - vecA;
+	return 0.5 * diff1.crossProduct(diff2).magnitude();
+}
+
+#endif
