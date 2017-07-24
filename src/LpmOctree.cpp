083f27fb702fc23d996ac4f8c47604488bb5ea18
Peter Bosler <peter.bosler@me.com>
2017-07-24 16:40:28 -0600

    emergency commit -- computer is crashing.

--- src/LpmOctree.cpp
+++ src/LpmOctree.cpp
@@ -14,30 +14,43 @@ namespace Lpm {
 
 std::unique_ptr<Logger> Tree::log(new Logger(OutputMessage::debugPriority, "Tree_log"));
 
-Tree::Node::Node() : box(Box3d()), maxAspectRatio(1.0), level(-1), coordsContained(std::vector<index_type>()), 
-    parent(NULL), kids(std::vector<std::shared_ptr<Treenode>>()) {}
-
-Tree::Tree(const Coords* crds, const scalar_type maxRatio) : 
-    box(crds->minX() - BOX_PADDING_FACTOR * crds->minX(), 
-        crds->maxX() + BOX_PADDING_FACTOR * crds->maxX(),
-        crds->minY() - BOX_PADDING_FACTOR * crds->minY(), 
-        crds->maxY() + BOX_PADDING_FACTOR * crds->maxY(), 
-        crds->minZ() - BOX_PADDING_FACTOR * crds->minZ(), 
-        crds->maxZ() + BOX_PADDING_FACTOR * crds->maxZ()),
-    coordsContained(crds->n(), -1), maxAspectRatio(maxRatio), level(0), 
-    parent(std::weak_ptr<Treenode>()), kids(std::vector<std::shared_ptr<Treenode>>()) {
+Tree::Tree(const Coords* crds, const scalar_type maxRatio, const int prank) : _crds(crds), _maxAspectRatio(maxRatio),
+    _depth(0), _nnodes(1) {
+    log->setProcRank(prank);
+    
+    Box3d rbox(crds->minX() - BOX_PADDING_FACTOR * crds->minX(), 
+      crds->maxX() + BOX_PADDING_FACTOR * crds->maxX(),
+      crds->minY() - BOX_PADDING_FACTOR * crds->minY(), 
+      crds->maxY() + BOX_PADDING_FACTOR * crds->maxY(), 
+      crds->minZ() - BOX_PADDING_FACTOR * crds->minZ(), 
+      crds->maxZ() + BOX_PADDING_FACTOR * crds->maxZ());
+            
+    std::vector<index_type> rinds;
+    rinds.reserve(crds->n());
     for (index_type i = 0; i < crds->n(); ++i) {
-        coordsContained[i] = i;
+        rinds.push_back(i);
     }
+    
+    Node* rparent = NULL;
+    
+    root = std::unique_ptr<Node>(new Node(rbox, rparent, rinds));
 }
 
-std::string Treenode::infoString() const {
+std::string Tree::infoString() const {
+    std::stringstream ss;
+    ss << "Tree info:" << std::endl;
+    ss << "\tnNodes = " << _nnodes << std::endl;
+    ss << "\tdepth = " << _depth << std::endl;
+    ss << "\tmaxAspectRatio = " << _maxAspectRatio << std::endl;
+    return ss.str();
+}
+
+std::string Tree::Node::infoString() const {
     std::stringstream ss;
     ss << "Treenode info: " << std::endl;
     ss << "\tbox: " << box.infoString();
-    ss << "\tmaxAspectRatio = " << maxAspectRatio << std::endl;
     ss << "\tlevel = " << level << std::endl;
-    ss << "\tparent = " << (parent.use_count() > 0 ? parent.lock().get() : 0) << std::endl;
+    ss << "\t&parent = " << (level > 0 ? parent : 0) << std::endl;
     ss << "\thasKids = " << (hasKids() ? "true" : "false") << ", kids.size() = " << kids.size() << std::endl;
     ss << "\tcoordsContained.size() = " << coordsContained.size() << std::endl;
     if (!coordsContained.empty()) {
@@ -47,8 +60,15 @@ std::string Treenode::infoString() const {
     return ss.str();
 }
 
-Treenode::Treenode(const Box3d& bbox, const std::shared_ptr<Treenode> pparent, const std::vector<index_type>& crdInds, const scalar_type maxRatio) :
-    box(bbox), parent(pparent), coordsContained(crdInds), maxAspectRatio(maxRatio), level(pparent->level +1) {};
+Tree::Node::Node(const Box3d& bbox, const Node* pparent, const std::vector<index_type>& crdInds) :
+    box(bbox), parent(pparent), coordsContained(crdInds) {
+    if (pparent) {
+        level = pparent->level + 1;
+    }
+    else {
+        level = 0;
+    }
+}
 
 void generateTree(std::shared_ptr<Treenode> node, std::shared_ptr<Coords> crds, const index_type maxCoordsPerNode) {
     //
@@ -92,7 +112,7 @@ void generateTree(std::shared_ptr<Treenode> node, std::shared_ptr<Coords> crds, 
             //  only add non-empty children
             //
             if (!kidcoords.empty()) {
-                node->kids.push_back(std::shared_ptr<Treenode>(new Treenode(kids[i], node, kidcoords, node->maxAspectRatio)));
+                node->kids.push_back(std::shared_ptr<Treenode>(new Treenode(kids[i], node, kidcoords)));
             }
         }
         
@@ -109,6 +129,63 @@ void generateTree(std::shared_ptr<Treenode> node, std::shared_ptr<Coords> crds, 
         }
     }
 }
+
+void Tree::buildTree(std::unique_ptr<Tree::Node> node, const index_type maxCoordsPerNode) {
+    if (node->coordsContained.size() <= maxCoordsPerNode) {
+        return;
+    }
+    else {
+        //
+        //  determine box dimensions to split
+        //
+        bool splitDims[3];
+        int splitCount = 0;
+        const scalar_type edgeThreshold = node->box.longestEdge() / _maxAspectRatio;
+        for (int i = 0; i < 3; ++i) {
+            if (node->box.edgeLength(i) >= edgeThreshold) {
+                splitDims[i] = true;
+                splitCount += 1;
+            }
+            else {
+                splitDims[i] = false;
+            }
+        }
+        //
+        //  make child boxes
+        //
+        std::vector<Box3d> kidboxes = node->box.bisectAlongDims(splitDims);
+        for (int i = 0; i < kidboxes.size(); ++i) {
+            //
+            //  find coordinates contained by child box
+            //
+            std::vector<index_type> kidcoords;
+            kidcoords.reserve(node->coordsContained.size());
+            for (index_type j = 0; j < node->coordsContained.size(); ++j) {
+                if (kidboxes[i].containsPoint(_crds->getVec(node->coordsContained[j]))) {
+                    kidcoords.push_back(node->coordsContained[j]);
+                }
+            }
+            kidcoords.shrink_to_fit();
+            if (!kidcoords.empty()) {
+                node->kids.push_back(std::unique_ptr<Node>(new Node(kidboxes[i], node.get(), kidcoords)));
+            }
+        }
+        if (node->kids.empty()) {
+            OutputMessage errMsg("All kids are empty, this shouldn't happen.", OutputMessage::errorPriority, "Treenode::generateTree");
+            node->log->logMessage(errMsg);
+            return;
+        }
+        else {
+            _depth += 1;
+            _nnodes += node->kids.size();
+        }
+        for (int i = 0; i < node->kids.size(); ++i) {
+             shrinkBox(node->kids[i]);  
+             buildTree(node->kids[i], maxCoordsPerNode);
+        }
+    }
+}
+
 
 void writeTreeToVtk(const std::string& filename, const std::string& desc, const std::shared_ptr<Treenode> root) {
     std::ofstream fs(filename);
@@ -210,15 +287,15 @@ index_type nTreenodes(const std::shared_ptr<Treenode> node) {
 }
 
 
-void Treenode::shrinkBox(const std::shared_ptr<Coords>& crds) {
+void Tree::shrinkBox(std::unique_ptr<Node> node) {
     scalar_type xmin = std::numeric_limits<scalar_type>::max();
     scalar_type xmax = std::numeric_limits<scalar_type>::lowest();
     scalar_type ymin = xmin;
     scalar_type ymax = xmax;
     scalar_type zmin = xmin;
     scalar_type zmax = xmax;
-    for (index_type i = 0; i < coordsContained.size(); ++i) {
-        const XyzVector crdVec = crds->getVec(coordsContained[i]);
+    for (index_type i = 0; i < node->coordsContained.size(); ++i) {
+        const XyzVector crdVec = _crds->getVec(node->coordsContained[i]);
         if (crdVec.x < xmin)
             xmin = crdVec.x;
         if (crdVec.x > xmax)
@@ -232,12 +309,12 @@ void Treenode::shrinkBox(const std::shared_ptr<Coords>& crds) {
         if (crdVec.z > zmax)
             zmax = crdVec.z;
     }
-    box.xmin = xmin;
-    box.xmax = xmax;
-    box.ymin = ymin;
-    box.ymax = ymax;
-    box.zmin = zmin;
-    box.zmax = zmax;
+    node->box.xmin = xmin;
+    node->box.xmax = xmax;
+    node->box.ymin = ymin;
+    node->box.ymax = ymax;
+    node->box.zmin = zmin;
+    node->box.zmax = zmax;
 }
 
 }
--- src/LpmOctree.h
+++ src/LpmOctree.h
@@ -14,52 +14,63 @@
 namespace Lpm {
 
 class Tree {
-    Tree(const Coords* crds, const scalar_type maxAspectRatio = 1.0);
-
-    struct Node {
-        Node();
+    public:
+        Tree(const Coords* crds, const scalar_type maxAspectRatio = 1.0, const int prank = 0);
         
-        Node(const Box3d& bbox, const Node* pparent = NULL, 
-                 const std::vector<index_type>& crdInds = std::vector<index_type>(), const scalar_type maxAspectRatio = 1.0);
-
-        Box3d box;
-        scalar_type maxAspectRatio;
-        int level;
-        /// ptr to parent
-        Node* parent;
-        /// ptrs to child boxes
-        std::vector<std::unique_ptr_ptr<Node>> kids;
-        std::vector<index_type> coordsContained;
-    
-        inline index_type nCoords() const {return coordsContained.size();}
-    
-        void shrinkBox(const Coords* crds);
-    
-        bool hasKids() const {return (!kids.empty());}
-    
-        void writePoints(std::ofstream& os) const;
         inline void setLogProc(const int prank) {log->setProcRank(prank);}
-    
         std::string infoString() const;
-    };
+        
+        int treeDepth(const std::shared_ptr<Treenode> node);
+
+        index_type nTreenodes(const std::shared_ptr<Treenode> node);
+
+        
+
+        void writeTreeToVtk(const std::string& filename, const std::string& desc = "", const std::shared_ptr<Treenode> node = NULL);
+        void writeVTKPoints(std::ofstream& os, const std::shared_ptr<Treenode> node);
+        void writeVtkCells(std::ofstream& os, const std::shared_ptr<Treenode> node, index_type& vertIndex);
+        void writeVtkCellType(std::ofstream& os, const std::shared_ptr<Treenode> node);
+        void writeVtkLevelData(std::ofstream& os, const std::shared_ptr<Treenode> node);
+
+    protected:
+        struct Node {        
+            Node(const Box3d& bbox, const Node* pparent = NULL, 
+                 const std::vector<index_type>& crdInds = std::vector<index_type>());
+            
+            inline index_type nCoords() const {return coordsContained.size();}
     
-    std::unique_ptr<Node> root;
-    static std::unique_ptr<Logger> log;
-    inline void setLogProc(const int prank) {log->setProcRank(prank);}
-    std::string infoString() const;
+            void shrinkBox(const Coords* crds);
+    
+            bool hasKids() const {return (!kids.empty());}
+    
+            void writePoints(std::ofstream& os) const;
+            inline void setLogProc(const int prank) {log->setProcRank(prank);}
+    
+            std::string infoString() const;
+            
+            
+            Box3d box;
+            int level;
+            /// ptr to parent
+            Node* parent;
+            /// ptrs to child boxes
+            std::vector<std::unique_ptr_ptr<Node>> kids;
+            std::vector<index_type> coordsContained;
+        };
+        
+        void buildTree(std::unique_ptr<Node> node, const index_type maxCoordsPerNode);
+        
+//         void generate(std::shared_ptr<Treenode> node, std::shared_ptr<Coords> crds, const index_type maxCoordsPerNode);
+
+        int _depth;
+        index_type _nnodes;
+        std::weak_ptr<Coords> _crds;    
+        std::unique_ptr<Node> _root;
+        scalar_type _maxAspectRatio;
+        static std::unique_ptr<Logger> log;
 };
 
-int treeDepth(const std::shared_ptr<Treenode> node);
 
-index_type nTreenodes(const std::shared_ptr<Treenode> node);
-
-void generateTree(std::shared_ptr<Treenode> node, std::shared_ptr<Coords> crds, const index_type maxCoordsPerNode);
-
-void writeTreeToVtk(const std::string& filename, const std::string& desc = "", const std::shared_ptr<Treenode> node = NULL);
-void writeVTKPoints(std::ofstream& os, const std::shared_ptr<Treenode> node);
-void writeVtkCells(std::ofstream& os, const std::shared_ptr<Treenode> node, index_type& vertIndex);
-void writeVtkCellType(std::ofstream& os, const std::shared_ptr<Treenode> node);
-void writeVtkLevelData(std::ofstream& os, const std::shared_ptr<Treenode> node);
 }
 
 #endif
