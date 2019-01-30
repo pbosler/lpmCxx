f9704205fec8ef0f2b0a5fb2f15525153f15cfb6
Pete Bosler <pabosle@sandia.gov>
2019-01-30 15:56:15 -0700

    added bve particles.  fixed bug in coriolis formula.

--- src/LpmAosBVEParticle.hpp
+++ src/LpmAosBVEParticle.hpp
@@ -1,5 +1,5 @@
-#ifndef LPM_AOS_SWE_PARTICLE_HPP
-#define LPM_AOS_SWE_PARTICLE_HPP
+#ifndef LPM_AOS_BVE_PARTICLE_HPP
+#define LPM_AOS_BVE_PARTICLE_HPP
 
 #include "LpmAosTypes.hpp"
 #include "LpmConfig.h"
@@ -10,7 +10,7 @@
 namespace Lpm {
 namespace Aos {
 
-template <int ndim=3> class SWEParticle : public Particle<ndim> {
+template <int ndim=3> class BVEParticle : public Particle<ndim> {
     public:
         SWEParticle(const bool isvert=false) : 
         	Particle<ndim>(isvert) {
@@ -20,12 +20,8 @@ template <int ndim=3> class SWEParticle : public Particle<ndim> {
             else {
                 this->registerScalarField("velocity");
             }
-            this->registerScalarField("potvort");
+            this->registerScalarField("absvort");
             this->registerScalarField("relvort");
-            this->registerScalarField("divergence");
-            this->registerScalarField("depth");
-            this->registerScalarField("surf_height");
-            this->registerScalarField("bottom_height");
         }
 
         SWEParticle(const Vec<ndim>& pos, const scalar_type wgt = 0.0, const bool isvert=false) : 
@@ -36,12 +32,8 @@ template <int ndim=3> class SWEParticle : public Particle<ndim> {
             else {
                 this->registerScalarField("velocity");
             }
-            this->registerScalarField("potvort");
+            this->registerScalarField("absvort");
             this->registerScalarField("relvort");
-            this->registerScalarField("divergence");
-            this->registerScalarField("depth");
-            this->registerScalarField("surf_height");
-            this->registerScalarField("bottom_height");
         }
         
         SWEParticle(const Vec<ndim>& xx, const Vec<ndim>& aa, const scalar_type wgt = 0.0, const bool isvert=false) :
@@ -52,12 +44,8 @@ template <int ndim=3> class SWEParticle : public Particle<ndim> {
             else {
                 this->registerScalarField("velocity");
             }
-            this->registerScalarField("potvort");
+            this->registerScalarField("absvort");
             this->registerScalarField("relvort");
-            this->registerScalarField("divergence");
-            this->registerScalarField("depth");
-            this->registerScalarField("surf_height");
-            this->registerScalarField("bottom_height");
         }
         
         scalar_type calcPV(const scalar_type f) const {return (this->relvort + f)/this->depth;}
@@ -76,8 +64,6 @@ template <int ndim=3> class SWEParticle : public Particle<ndim> {
 inline scalar_type coriolis(const Vec<2>& pos, const scalar_type f0=0.0, const scalar_type beta=0.0) {return f0 + beta*pos.x[1];}
 
 inline scalar_type coriolis(const Vec<3>& pos, const scalar_type Omega=0.0) {return 2.0*Omega*std::sin(pos.latitude());}
-
-
 
 }
 }
